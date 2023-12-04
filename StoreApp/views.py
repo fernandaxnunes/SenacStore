033cef8565c6376f28c97150ba100e75ca53a436
Fernanda Nunes <nunesfnds546@gmail.com>
2023-12-04 19:21:12 -0300

    Configurando Templates

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from StoreApp.models import Departamento
+from StoreApp.models import Departamento, Produto
 
 # Create your views here.
 def index(request):
+     produtos_em_destaque = Produto.objects.filter(destaque = True)
+     context = {
+        'produtos' : produtos_em_destaque,
+    }
 
-    return render(request, 'index.html')
+     return render(request, 'index.html', context)
 
 def produto_lista(request):
-   
-    return render(request, 'produtos.html',)
+    
+    produtos = Produto.objects.all()
+    context = {
+        'produtos' : produtos,
+        'categoria' : 'Todos os produtos'
+    }
+    return render(request, 'produtos.html', context)
 
-def produto_detalhe(request):
+def produto_lista_por_id(request, id):
+    #Filtrando
+    produtos = Produto.objects.filter(departamento_id = id)
+    departamento = Departamento.objects.get(id = id)
 
-    return render(request, 'produto_detalhes.html')
+    context = {
+        'produtos' : produtos,
+        'categoria' : departamento.nome
+    }
+    return render(request, 'produtos.html', context)
+
+def produto_detalhe(request, id):
+    produto = Produto.objects.get(id = id)
+    context = {
+        'produto' : produto
+    }
+
+    return render(request, 'produto_detalhes.html',context)
